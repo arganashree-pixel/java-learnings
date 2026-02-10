8df652be2b8fedd926c5511f4a912fd1b679e4a1
arganashree-pixel <arganashree@gmail.com>
2026-02-10 15:32:17 +0530

    completed till the methods

---
+++ Exercise.java
@@ -0,0 +1,32 @@
+//Create  variables for your name(String),age(int),height(float),and weight(double).print all of them
+import java.lang.System;
+class Exercise{
+    public static void main(String[] args)
+    {
+        String name = "Vidyavahini";
+        int age = 20;
+        float height = 5.5f;
+        double weight = 60.5;
+        System.out.println("Name:"+name);
+        System.out.println("Age:"+age);
+        System.out.println("Height:"+height);
+        System.out.println("Weight:"+weight);
+    }
+}
+
+
+//Create two integer variables a=15 and b= 25.print their sum,difference,product,and division.
+class Exercise1{
+    public static void main(String[] args){
+        int a = 15;
+        int b = 25;
+        int sum = a + b;
+        int difference = a - b;
+        int product = a * b;
+        double division = (double)a/b;
+        System.out.println("Sum:"+sum);
+        System.out.println("Difference:"+difference);
+        System.out.println("Product:"+product);
+        System.out.println("Division:"+division);
+    }
+}
---
+++ methods.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+class Example1 {
+    void hello(){
+        System.out.println("Hello");
+    }
+    void add(int a, int b){
+        System.out.println(a + b);
+    }
+    public  static void main(String[] args){
+        Example1 obj = new Example1();
+        obj.add(10,30); // output:Hello
+    }
+}
+
+class Example3{
+    int getNumber(){
+        return 10;
+    }
+    public static void main(String[] args){
+        Example3 obj = new Example3();
+        int result = obj.getNumber();
+        System.out.println(result);
+    }
+}
+
+class Example4 {
+    static void staticMethod(){
+        System.out.println("I am static");
+    }
+    void instanceMethod(){
+        System.out.println("I am instance");
+    }
+    public static void main(String[] args){
+    staticMethod();
+    Example4 obj = new Example4();
+    obj.instanceMethod();
+}
+}
+
+class Example6{
+    void greet(String name){
+    System.out.println("Hello" + name);
+    }
+    public static void main(String[] args){
+        Scanner scn = new Scanner(System.in);
+        System.out.print("Enter name:");
+        String name = scn.nextLine();
+        Example6 obj = new Example6();
+        obj.greet(name);
+    }
+}
