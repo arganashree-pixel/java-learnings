a2df88aca9755875a7ca575484b4a7e807eb31a9
arganashree-pixel <arganashree@gmail.com>
2026-02-11 16:09:20 +0530

    completed till jagged array

---
+++ practice.java
@@ -0,0 +1,16 @@
+class gana{
+    public static void main(String[] args){
+        //print the first element
+        int[] num = {10,20,30,40,50};
+        {
+            System.out.println(num[0]);
+        }
+    }
+}
+
+class demo{
+    public static void main(String[] args){
+        int[][] num = {{10,20},{30,40}};
+        System.out.println(num[1][1]);
+    }
+}
